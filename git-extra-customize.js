bc9dad4eb24925a728bbd1ba0021e4719a9d44f2
John Lyon-Smith <john@lyon-smith.org>
2020-05-29 15:24:12 -0700

    Adding customize script

---
+++ git-extra-customize.js
@@ -0,0 +1,32 @@
+// This are the customization rules for use by the @johnls/git-extra tool
+
+var params = await ui.prompts([
+  {
+    name: "projectName",
+    message: "Project name?",
+    regex: "[a-z-][a-z0-9-]*",
+    error: "Name must be supplied and be in param-case (all lowercase)",
+  },
+  {
+    name: "gitUser",
+    message: "GitHub user name?",
+    regex: "^[a-zd](?:[a-zd]|-(?=[a-zd])){0,38}$", // From https://github.com/shinnn/github-username-regex
+    error: "Must be a valid GitHub user name",
+  },
+])
+
+params.projectNamePascal = name.pascal(params.projectName)
+
+let packageJson = await fs.readFile("package.json")
+
+packageJson = packageJson
+  .replace(/phaser-template/g, params.projectName)
+  .replace(/some-user/g, params.gitUser)
+
+await fs.writeFile("package.json", packageJson)
+
+await fs.mkdir("scratch")
+await fs.ensureFile("scratch/.gitkeep")
+await git.forceAdd("scratch/.gitkeep")
+
+await fs.mkdir(".vscode")
