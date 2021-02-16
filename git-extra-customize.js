de04479c6042bf2eec4630f8ac126b0ff79fe8a6
John Lyon-Smith <john@lyon-smith.org>
2021-02-16 10:17:14 -0800

    Update initial values for prompts

--- git-extra-customize.js
+++ git-extra-customize.js
@@ -3,25 +3,27 @@
 var params = await ui.prompts([
   {
     name: "projectName",
+    initial: args.projectName,
     message: "Project name?",
     regex: "[a-z-][a-z0-9-]*",
     error: "Name must be supplied and be in param-case (all lowercase)",
   },
   {
-    name: "gitUser",
+    name: "userName",
+    initial: args.userName,
     message: "GitHub user name?",
     regex: "^[a-zd](?:[a-zd]|-(?=[a-zd])){0,38}$", // From https://github.com/shinnn/github-username-regex
     error: "Must be a valid GitHub user name",
   },
 ])
 
-params.projectNamePascal = name.pascal(params.projectName)
+params.projectNamePascal = changeCase.pascal(params.projectName)
 
 let packageJson = await fs.readFile("package.json")
 
 packageJson = packageJson
   .replace(/phaser-template/g, params.projectName)
-  .replace(/some-user/g, params.gitUser)
+  .replace(/some-user/g, params.userName)
 
 await fs.writeFile("package.json", packageJson)
 
